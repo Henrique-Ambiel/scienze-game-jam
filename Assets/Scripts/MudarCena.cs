8e5c86a3d58e13441442c7f49fc8790ca073731a
Vegetableglass <kaiosouto10@gmail.com>
2026-07-10 17:21:36 -0300

    Adicionando cena Final, de GameOver e de Creditos

---
+++ Assets/Scripts/MudarCena.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MudarCena : MonoBehaviour
+{
+    public void CarregarCena(string nomeDaCena)
+    {
+        SceneManager.LoadScene(nomeDaCena);
+    }
+    
+    public void Sair()
+    {
+        Application.Quit();
+    }
+}
