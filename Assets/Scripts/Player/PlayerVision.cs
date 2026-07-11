cf42812d58ae990083f98a94931824d9d8f8333c
Henrique-Ambiel <146786119+Henrique-Ambiel@users.noreply.github.com>
2026-07-10 22:36:48 -0300

    Mecânica de troca de corpo e efeito vignette
    Mecânica de troca de personagem finalizada, jogador pode trocar de forma apertando a tecla espaço. Além disso foi adicionado o efeito de Vignette para causar a dificuldade de visualizar o entorno quando está na forma base e quando vira pássaro o efeito some.

--- Assets/Scripts/Player/PlayerVision.cs
+++ Assets/Scripts/Player/PlayerVision.cs
@@ -1,35 +1,52 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PlayerVision : MonoBehaviour
 {
-    public bool birdForm = false;
-
     public float oldWomanZoom = 3.5f;
     public float birdZoom = 5.5f;
 
-    public float zoomSpeed = 3f;
+    public float zoomSpeed = 3;
 
     private Camera cam;
+
+    bool birdForm;
+
+    public Volume volume;
+
+    private Vignette vignette;
 
     void Start()
     {
         cam = Camera.main;
+        volume.profile.TryGet(out vignette);
+    }
+
+    public void SetBirdForm(bool bird)
+    {
+        birdForm = bird;
     }
 
     void Update()
     {
-        // Apenas para teste
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            birdForm = !birdForm;
-        }
-
-        float targetZoom = birdForm ? birdZoom : oldWomanZoom;
+        float targetZoom = birdForm ?
+            birdZoom :
+            oldWomanZoom;
 
         cam.orthographicSize = Mathf.Lerp(
             cam.orthographicSize,
             targetZoom,
             Time.deltaTime * zoomSpeed
         );
+
+        float targetIntensity = birdForm ? 0f : 0.8f;
+
+        vignette.intensity.value =
+            Mathf.Lerp(
+                vignette.intensity.value,
+                targetIntensity,
+                Time.deltaTime * 4
+            );
     }
 }
